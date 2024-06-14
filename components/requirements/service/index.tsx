2bf6ef333594bad6fae24b67c08f2dd7f71b2421
Frederic Chien <fred_chien@yahoo.com.br>
2024-06-14 17:26:00 -0300

    fix: adjusts css

--- components/requirements/service/index.tsx
+++ components/requirements/service/index.tsx
@@ -2,7 +2,7 @@ export const data = [
   {
     prerequisites: [
       "Ter entre 16 e 35 anos de idade",
-      "Renda familiar até 3 salários mínimos por pessoa",
+      "Renda familiar até 3 salários mínimos",
       "De qualquer lugar do país",
       "Disponibilidade de 2 horas por dia, de segunda-feira à sexta-feira",
       "Possuir computador ou notebook",
